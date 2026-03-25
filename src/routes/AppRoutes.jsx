e683ff22bd3b9e1163a81e24c6bbaaf4a319837d
umoren victor <umorenismvic@gmail.com>
2026-03-25 07:35:22 -0700

    bottom icon top

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
-import Home from "../pages/Home";
+import {Home }from "../pages/Home";
 import {Menu }from "../pages/Menu";
 import Services from "../pages/Services";
 import About from "../pages/About"
